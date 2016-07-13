1f498b4135df44c87bbfd727a3d844b66e45aa8f
Daniel Lanza Garcia <daniel.lanza@cern.ch>
2016-07-13 17:33:08 +0200

    Remove 3_meetup_to_hdfs

--- 2_hadoop_tutorial/3_meetup_to_hdfs/conf/flume-env.sh
+++
@@ -1,9 +0,0 @@
-
-JAVA_HOME=/usr/lib/jvm/jre-openjdk
-
-# Give Flume more memory and pre-allocate, enable remote monitoring via JMX
-JAVA_OPTS="-Xms20m -Xmx50m -Duser.timezone=Europe/Zurich"
-
-# Limit vmem usage
-# https://issues.apache.org/jira/browse/HADOOP-7154
-export MALLOC_ARENA_MAX=4
