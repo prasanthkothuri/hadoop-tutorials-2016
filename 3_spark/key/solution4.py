c752212f2082bf9cfbc554849f3c6e5121ac572a
Prasanth Kothuri <pkothuri@cern.ch>
2016-08-02 15:17:50 +0200

    correct minor errors

--- 3_spark/key/solution4.py
+++ 3_spark/key/solution4.py
@@ -1,4 +1,4 @@
-mbda record: record.Sex != '"Both"') \
+ntuple_rdd.filter(lambda record: record.Sex != '"Both"') \
     .map(lambda record: ((record.Location,record.Time,record.Sex),float(record.Value))) \
     .reduceByKey(add) \
     .map(lambda record: ((record[0][0],record[0][1]),(record[0][2],record[1]))) \
